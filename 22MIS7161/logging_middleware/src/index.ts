8f7f964ce9bfea0e7fc09b4fb7beb3e58099d318
KilariDimple <kilaridimple@gmail.com>
2026-05-16 17:42:41 +0530

    chore: cleanup

--- 22MIS7161/logging_middleware/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { Log, LogSync } from './utils/logger';
-export { getTok, clearTokCache } from './services/authSvc';
-export { sendLog } from './services/logSvc';
-export { getCfg } from './config/env';
-
-export * from './types/log';
-export * from './constants/vals';
